39dea41c26bfcb775a5ba35fd3164a1049cbe05a
Sable Ayala <sable262021@gmail.com>
2025-01-12 18:14:42 -0500

    added flakes

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        #packages = with pkgs; [
+        #  git
+        #  ghc
+        #  haskellPackages.cabal-install
+        #];
+
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.git
+            pkgs.ghc
+            pkgs.haskellPackages.cabal-install
+          ];
+        };
+      } );
+}
